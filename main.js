ec92c11c95cbc63077f64fcb7d50c618c522d1f2
Adriana <am20dipi@alum.siena.edu>
2021-05-21 15:25:06 -0400

    Done.

--- main.js
+++ main.js
@@ -6,6 +6,51 @@ const FULL_HEART = '♥'
 
 
 
+const likeIcon = document.querySelector(".like-glyph");
+const likeBtn = document.querySelector(".like");
+const activatedHeart = document.querySelector(".activated-heart");
+//likeClass = document.querySelector(".like")
+
+
+//button clicked
+
+let clicked = false;
+likeIcon.addEventListener("click", () => {
+  if (!clicked) {
+    clicked = true;
+    likeBtn.innerHTML = activatedHeart.value;
+    likeBtn.style.color = "#FF69B4";
+  } else {
+    clicked = false;
+  };
+};
+
+function handleClick(e) {
+  const heart = e.target;
+  mimicServerCall("http://mimicServer.example.com")
+  .then(function(response) {
+    heart.innerHTML = activatedHeart.innerHTML;
+    heart.style.color = "#FF69B4";
+  })
+  .catch(function(error) {
+    const modal = document.getElementById('hidden')
+    // declaring and assigning a variable "modal"
+    // querying the document to select all elements with an id of "hidden"
+    modal.className = "";
+    // resetting className of Modal to be empty string
+    modal.innerText = "Error!";
+    // changing innerText of modal to be "Error!"
+    setTimeout(() => modal.className = "hidden", 5000);
+    // setting Timeout so error does not occur immediately, changing className to "hidden"
+  });
+}
+
+
+
+
+
+
+
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
